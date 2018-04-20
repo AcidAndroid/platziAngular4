19b5f26d69a9ba129729ed7e19c07ebb50155616
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2018-04-20 16:40:53 -0500

    directivas

--- platzisquare2/src/app/app.component.ts
+++ platzisquare2/src/app/app.component.ts
@@ -7,25 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PlatziSquare!';
-  listo = false;
-  listo2 = false;
-  stringInterpolation ="Soy string interpolation binding";
-   nombre="";
-
-   name=""
-   apPaterno=""
-   apMaterno=""
-
+ lugares:any =[
+   {active:true , nombre:"Floreria la primavera"}
+   ,{active:false , nombre:"Krispy Kreme"}
+   ,{active:true , nombre:"Loncheria Marrano"}
+   ,{active:true , nombre:"Veterinaria perro agradecido"}
+   ,{active:false , nombre:"Sushi star"}
+ ]
   constructor(){
-    setTimeout(()=>{this.listo=true;},5000);
-    setInterval(()=>{this.listo2=!this.listo2},2000);  
+    
   }
 
-   mehanclicado() {
-    alert('Me tocaste!!');
-  }
-
-  enviodatos(){
-    alert(`Enviando tus datos...${this.name} ${this.apPaterno} ${this.apMaterno}`)
-  }
+   
 }
